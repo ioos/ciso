e2a86a89dc001c1c8f5e2ec2f62e56f50e2b084e
Filipe Fernandes <ocefpaf@gmail.com>
2026-01-13 11:41:06 -0300

    implement ABI3 and drop py<311

--- setup.py
+++ setup.py
@@ -4,12 +4,24 @@ import numpy as np
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
+DEFINE_MACROS = [
+    ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION"),
+    # 0x030B0000 -> 3.11
+    ("Py_LIMITED_API", "0x030B0000"),
+    ("CYTHON_LIMITED_API", None),
+]
+
 extensions = [
     Extension(
         "ciso._ciso",
         ["ciso/_ciso.pyx"],
         include_dirs=[np.get_include()],
+        define_macros=DEFINE_MACROS,
+        py_limited_api=True,
     ),
 ]
 
-setup(ext_modules=cythonize(extensions))
+setup(
+    ext_modules=cythonize(extensions),
+    options={"bdist_wheel": {"py_limited_api": "cp311"}},
+)
